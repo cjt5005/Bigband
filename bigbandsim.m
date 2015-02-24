09ec13221fb19a73fcfee9b1f9a96b0e4d1e6a47
cjt5005 <cjt5005@gmail.com>
2015-02-24 16:37:20 -0500

    Removed "phase shift experiment" from bigbandsim. created separate file for phase shift experiment

--- bigbandsim.m
+++ bigbandsim.m
@@ -28,8 +28,8 @@ timeDelay = wrapTime;
 
 t2 = t1+timeDelay;
 
-s1 = sin(2*pi*f*t1);
-s2 = sin(2*pi*f*t2);
+s1 = exp(2i*pi*f*t1);
+s2 = exp(2i*pi*f*t2);
 
 plot(s1)
 hold on
@@ -38,9 +38,9 @@ plot(s2)
 s1d = s1(1:p:end);
 s2d = s2(1:p:end);
 figure()
-plot(s1d)
+plot(imag(s1d))
 hold on
-plot(s2d)
+plot(imag(s2d),'r')
 
 %% Signal Model
 t = (0:nfft*p-1)*1/effectiveFs; %Time vector for non-aliased signal
